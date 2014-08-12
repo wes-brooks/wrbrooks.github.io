afdade8dc2bc868440f1b9181017be324e6193ef
Wesley Brooks <brooks.wesley@gmail.com>
2014-08-11 19:25:22 -0500

    added highslide

---
+++ highslide/highslide.config.js
@@ -0,0 +1,25 @@
+/**
+*	Site-specific configuration settings for Highslide JS
+*/
+hs.graphicsDir = 'highslide/graphics/';
+hs.showCredits = false;
+hs.outlineType = 'custom';
+hs.dimmingOpacity = 0.75;
+hs.align = 'center';
+hs.captionEval = 'this.a.title';
+hs.captionOverlay.position = 'below';
+
+
+// Add the slideshow controller
+hs.addSlideshow({
+	slideshowGroup: 'group1',
+	interval: 5000,
+	repeat: false,
+	useControls: false
+});
+
+// gallery config object
+var config1 = {
+	slideshowGroup: 'group1',
+	transitions: ['expand', 'crossfade']
+};
